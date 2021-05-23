fa95a0b5d215a38f9ae8b00db0f4a08f60f61113
Andrew Graham <queglay@gmail.com>
2021-05-23 15:49:33 +0930

    optional-vars

--- variables.tf
+++ variables.tf
@@ -11,6 +11,28 @@ variable "common_tags" {
 }
 variable "name" {
   description = "The name of the security group"
-  default = "consul_client"
-  type = string
+  default     = "consul_client"
+  type        = string
+}
+
+variable "allowed_inbound_cidr_blocks" {
+  description = "A list of CIDR-formatted IP address ranges from which the EC2 Instances will allow connections to Consul"
+  type        = list(string)
+  default     = []
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "allowed_inbound_security_group_ids" {
+  description = "A list of security group IDs that will be allowed to connect to Consul"
+  type        = list(string)
+  default     = []
+}
+
+variable "allowed_inbound_security_group_count" {
+  description = "The number of entries in var.allowed_inbound_security_group_ids. Ideally, this value could be computed dynamically, but we pass this variable to a Terraform resource's 'count' property and Terraform requires that 'count' be computed with literals or data sources only."
+  default     = 0
 }
